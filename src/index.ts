8d8c5e311a1d239567112ba9b383313cc1130649
Lukas Hermann <hey@lukashermann.dev>
2024-09-23 12:56:50 +0200

    docs: add legal notice

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
+/*!
+ * @rundown-studio/utils
+ * © Rundown Studio <hello@rundownstudio.app>
+ * All rights reserved.
+ */
+
 export * from './parseSnapshot'
 export * from './createTimestamps'
 export * from './fastDeepEqual'
